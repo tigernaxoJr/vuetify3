766dce856d5acc6bc4ba76d80e36f903c00d9e67
Yu-Cheng,Chen <tigernaxo@gmail.com>
2023-04-10 09:49:42 +0800

    tmp

--- src/components/SideMenu.d.ts
+++ src/components/SideMenu.d.ts
@@ -4,11 +4,13 @@ import { DefineComponent } from "vue";
 //#region Menus
 // Menus 把所有屬性都丟給 VListItem，因此可透過 Menus 控制 VListItem
 // 因此 Menus 拿 VListItem 的 $props 作為屬性，並額外加入屬性 id, children
-type MenusExtra = {
-  id: any,
-  children: VListItem['$props']
-}
-export type Menus = Array<VListItem['$props'] & MenusExtra>;
+type MenusBase = Pick<
+  VListItem['$props'],
+  Exclude<keyof VListItem['$props'], `v-${string}` | `$${string}`>
+>
+type MenusExtra = { id: any, }
+type MenusUnit = MenusBase & MenusExtra & { children?: MenusUnit }
+export type Menus = Array<MenusUnit>;
 //#endregion
 
 //#region IProps
