0b309605342615c28cdc7df8d4aac22e326cd3d0
Yu-Cheng,Chen <tigernaxo@gmail.com>
2023-06-06 09:02:54 +0800

    dayx

--- src/utils/dayx.js
+++ src/utils/dayx.js
@@ -1,33 +1,29 @@
 import day from 'dayjs'
-const ktyyy = (dt)=>(dt.getFullYear() - 1911).toString().padStart(3, '0')
+const rocyyy = (dt)=>(dt.getFullYear() - 1911).toString().padStart(3, '0')
 const prototype = Object.getPrototypeOf(day())
 const handler = {
   get: function (target, prop, receiver) {
-    // age
+    // age, dte, tme
     if (prop === 'age') return day().diff(receiver, 'years', false)
     if (prop === 'dte') return receiver.format('YYYMMDD')
     if (prop === 'tme') return receiver.format('HHmmss')
     // format
-    if (prop === 'format') {
-      return function (...args) {
-        const format = args[0]
+    if (prop === 'format') return (format) =>{
         const formattingTokens = /Y{4,}|Y{3}|[^Y{3}]+/g
         const arr = format.match(formattingTokens)
         const ktformat = arr
-          .map(x => x === 'YYY' ? ktyyy(target.$d) : x)
+          .map(x => x === 'YYY' ? rocyyy(target.$d) : x)
           .join('')
-        return new day(target.$d).format(ktformat)
+        return target.format(ktformat)
       }
-    }
 
-    // Check if the property is a function
-    // Return the original property value
+    // Property is not function, else condition
     if (typeof target[prop] !== 'function') return target[prop]
-    // Wrap the original function with custom behavior
+    // Property is function, Wrap it
     return function (...args) {
       const result = target[prop].apply(target, args)
-      if (Object.getPrototypeOf(result) === prototype) return p(result, handler)
-      return result
+      const isReturnDayjs = Object.getPrototypeOf(result) === prototype
+      return isReturnDayjs ? p(result, handler) : result
     }
   },
   set(obj, prop, v, receiver) {
@@ -40,15 +36,13 @@ const handler = {
       return receiver
     }
     if (prop === 'tme') {
-      console.log('setting tme', obj,prop,v)
       v = v.padEnd(6, '0')
       obj.$d.setHours(parseInt(v.substring(0, 2)))
       obj.$d.setMinutes(parseInt(v.substring(2, 4)))
       obj.$d.setSeconds(parseInt(v.substring(4, 6)))
       return receiver
-    } else {
-      return Reflect.set(...arguments)
     }
+    return Reflect.set(...arguments)
   },
 }
 const p = (...args) => new Proxy(day(...args), handler)
