7d50a0f9826840224f6d8f324e8e5e1a534cc4af
tigernaxoJr <tigernaxoJr@users.noreply.github.com>
2023-06-06 20:20:56 +0000

    deploy: b232e5fe41fbdd5680a9313ad8b049721d791260

---
+++ assets/VContainer.bd4f8f5e.js
@@ -0,0 +1,1 @@
+/* empty css              */import{m as t,u as n}from"./color.bf68bd88.js";import{g as r,c as s}from"./index.3f00c8dc.js";const p=r()({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...t()},setup(e,a){let{slots:o}=a;return n(()=>s(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},o)),{}}});export{p as V};
